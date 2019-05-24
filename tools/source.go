6bc479b484595c42aa30c0e00285b07ae0a4b94b
Sean McGinnis <sean.mcginnis@gmail.com>
2019-05-24 08:06:17 -0500

    Add helper script for generating source files
    This adds a script to read the schema definitions and generate a source
    file from the results. Typical use is:
    
        python ./generate_from_schema.py Volume > volume.go
        gofmt -w volume.go
    
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>

---
+++ tools/source.go
@@ -0,0 +1,112 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package swordfish
+
+import (
+	"encoding/json"
+
+        "github.com/stmcginnis/gofish/school/common"
+)
+
+// Default{{ object_name }}Path is the default URI for the {{ object_name }}
+// object.
+const Default{{ object_name }}Path = "/redfish/v1/{{ object_name }}"
+{% for enum in enums %}
+
+{{ enum.description }}
+type {{ enum.name }} string
+
+const (
+{% for enum_member in enum.members %}
+    {{ enum_member.description }}
+    {{ enum_member.name }}{{ enum.name }} {{ enum.name }} = "{{ enum_member.name }}"
+{%- endfor %}
+)
+{% endfor %}
+{% for class in classes -%}
+
+{{ class.description }}
+type {{ class.name }} struct {
+    common.Entity
+{% for attr in class.attrs %}
+    {{ attr.description }}
+    {{ attr.name }}  {{ attr.type }}
+{%- endfor %}
+}
+
+// UnmarshalJSON unmarshals a {{ class.name }} object from the raw JSON.
+func ({{ class.name|lower }} *{{ class.name }}) UnmarshalJSON(b []byte) error {
+    type temp {{ class.name }}
+    var t struct {
+        temp
+    }
+
+    err := json.Unmarshal(b, &t)
+    if err != nil {
+        return err
+    }
+
+    *{{ class.name|lower }} = {{ class.name }}(t.temp)
+
+    // Extract the links to other entities for later
+
+    return nil
+}
+
+{% if class.name == object_name %}
+// Get{{ class.name }} will get a {{ class.name }} instance from the service.
+func Get{{ class.name }}(c common.Client, uri string) (*{{ class.name }}, error) {
+    resp, err := c.Get(uri)
+    defer resp.Body.Close()
+
+    var {{ class.name|lower }} {{ class.name }}
+    err = json.NewDecoder(resp.Body).Decode(&{{ class.name|lower }})
+    if err != nil {
+        return nil, err
+    }
+
+    {{ class.name|lower }}.SetClient(c)
+    return &{{ class.name|lower }}, nil
+}
+
+// ListReferenced{{ class.name }}s gets the collection of {{ class.name }} from
+// a provided reference.
+func ListReferenced{{ class.name }}s(c common.Client, link string) ([]*{{ class.name }}, error) {
+    var result []*{{ class.name }}
+    if link == "" {
+        return result, nil
+    }
+
+    links, err := common.GetCollection(c, link)
+    if err != nil {
+        return result, err
+    }
+
+    for _, {{ class.name|lower }}Link := range links.ItemLinks {
+        {{ class.name|lower }}, err := Get{{ class.name }}(c, {{ class.name|lower }}Link)
+        if err != nil {
+            return result, err
+        }
+        result = append(result, {{ class.name|lower }})
+    }
+
+    return result, nil
+}
+
+// List{{ class.name }}s gets all {{ class.name }} in the system.
+func List{{ class.name }}s(c common.Client) ([]*{{ class.name }}, error) {
+    return ListReferenced{{ class.name }}s(c, Default{{ class.name }}Path)
+}
+
+{% endif %}
+{% endfor %}
