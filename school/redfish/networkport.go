02c5865c51fbfbf74ccf460c2a44f4c9af645b6f
Sean McGinnis <sean.mcginnis@gmail.com>
2019-08-08 07:54:29 -0500

    Add NetworkPort unit test coverage

--- school/redfish/networkport.go
+++ school/redfish/networkport.go
@@ -87,36 +87,40 @@ const (
 	EEESupportedEthernetCapabilities SupportedEthernetCapabilities = "EEE"
 )
 
-// NetDevFuncMaxBWAlloc shall describe a maximum bandwidth
-// percentage allocation for a network device function associated with a
-// port.
+// NetDevFuncMaxBWAlloc shall describe a maximum bandwidth percentage allocation
+// for a network device function associated with a port.
 type NetDevFuncMaxBWAlloc struct {
-	// MaxBWAllocPercent shall be the maximum
-	// bandwidth percentage allocation for the associated network device
-	// function.
+	// MaxBWAllocPercent shall be the maximum bandwidth percentage allocation
+	// for the associated network device function.
 	MaxBWAllocPercent int
-	// NetworkDeviceFunction shall be a
-	// reference of type NetworkDeviceFunction that represents the Network
-	// Device Function associated with this bandwidth setting of this Network
-	// Port.
-	NetworkDeviceFunction string
+	// NetworkDeviceFunction shall be a reference of type NetworkDeviceFunction
+	// that represents the Network Device Function associated with this
+	// bandwidth setting of this Network Port.
+	NetworkDeviceFunction NetworkDeviceFunction
 }
 
-// NetDevFuncMinBWAlloc shall describe a minimum bandwidth
-// percentage allocation for a network device function associated with a
-// port.
+// NetDevFuncMinBWAlloc shall describe a minimum bandwidth percentage allocation
+// for a network device function associated with a port.
 type NetDevFuncMinBWAlloc struct {
-	// MinBWAllocPercent shall be the minimum
-	// bandwidth percentage allocation for the associated network device
-	// function. The sum total of all minimum percentages shall not exceed
-	// 100.
+	// MinBWAllocPercent shall be the minimum bandwidth percentage allocation
+	// for the associated network device function. The sum total of all minimum
+	// percentages shall not exceed 100.
 	MinBWAllocPercent int
-	// NetworkDeviceFunction shall be a
-	// reference of type NetworkDeviceFunction that represents the Network
-	// Device Function associated with this bandwidth setting of this Network
-	// Port.
-	NetworkDeviceFunction string
+	// NetworkDeviceFunction shall be a reference of type NetworkDeviceFunction
+	// that represents the Network Device Function associated with this
+	// bandwidth setting of this Network Port.
+	NetworkDeviceFunction NetworkDeviceFunction
 }
+
+// PortLinkStatus is the port link status.
+type PortLinkStatus string
+
+const (
+	// UpPortLinkStatus The port is enabled and link is good (up).
+	UpPortLinkStatus PortLinkStatus = "Up"
+	// DownPortLinkStatus  The port is enabled but link is down.
+	DownPortLinkStatus PortLinkStatus = "Down"
+)
 
 // NetworkPort represents a discrete physical port capable of connecting to a
 // network.
@@ -131,33 +135,28 @@ type NetworkPort struct {
 	ODataID string `json:"@odata.id"`
 	// ODataType is the odata type.
 	ODataType string `json:"@odata.type"`
-	// Actions is The Actions property shall contain the available actions
-	// for this resource.
-	Actions string
+	// Actions shall contain the available actions for this resource.
+	actions string
 	// ActiveLinkTechnology shall be the
 	// configured link technology of this port.
 	ActiveLinkTechnology LinkNetworkTechnology
-	// AssociatedNetworkAddresses shall be an
-	// array of configured network addresses that are associated with this
-	// network port, including the programmed address of the lowest numbered
-	// network device function, the configured but not active address if
-	// applicable, the address for hardware port teaming, or other network
-	// addresses.
+	// AssociatedNetworkAddresses shall be an array of configured network
+	// addresses that are associated with this network port, including the
+	// programmed address of the lowest numbered network device function, the
+	// configured but not active address if applicable, the address for hardware
+	// port teaming, or other network addresses.
 	AssociatedNetworkAddresses []string
-	// CurrentLinkSpeedMbps shall be the
-	// current configured link speed of this port.
+	// CurrentLinkSpeedMbps shall be the current configured link speed of this
+	// port.
 	CurrentLinkSpeedMbps int
 	// Description provides a description of this resource.
 	Description string
-	// EEEEnabled shall be a boolean indicating
-	// whether IEEE 802.3az Energy Efficient Ethernet (EEE) is enabled for
-	// this network port.
+	// EEEEnabled shall be a boolean indicating whether IEEE 802.3az Energy
+	// Efficient Ethernet (EEE) is enabled for this network port.
 	EEEEnabled bool
-	// FCFabricName shall indicate the FC Fabric Name
-	// provided by the switch.
+	// FCFabricName shall indicate the FC Fabric Name provided by the switch.
 	FCFabricName string
-	// FCPortConnectionType shall be the
-	// connection type for this port.
+	// FCPortConnectionType shall be the connection type for this port.
 	FCPortConnectionType PortConnectionType
 	// FlowControlConfiguration shall be the
 	// locally configured 802.3x flow control setting for this network port.
@@ -166,35 +165,29 @@ type NetworkPort struct {
 	// flow control behavior negotiated with the link partner for this
 	// network port (Ethernet-only).
 	FlowControlStatus FlowControl
-	// LinkStatus shall be the link status
-	// between this port and its link partner.
-	LinkStatus LinkStatus
-	// MaxFrameSize shall be the maximum frame
-	// size supported by the port.
+	// LinkStatus shall be the link status between this port and its link
+	// partner.
+	LinkStatus PortLinkStatus
+	// MaxFrameSize shall be the maximum frame size supported by the port.
 	MaxFrameSize int
-	// NetDevFuncMaxBWAlloc shall be an array
-	// of maximum bandwidth allocation percentages for the Network Device
-	// Functions associated with this port.
+	// NetDevFuncMaxBWAlloc shall be an array of maximum bandwidth allocation
+	// percentages for the Network Device Functions associated with this port.
 	NetDevFuncMaxBWAlloc []NetDevFuncMaxBWAlloc
-	// NetDevFuncMinBWAlloc shall be an array
-	// of minimum bandwidth percentage allocations for each of the network
-	// device functions associated with this port.
+	// NetDevFuncMinBWAlloc shall be an array of minimum bandwidth percentage
+	// allocations for each of the network device functions associated with this
+	// port.
 	NetDevFuncMinBWAlloc []NetDevFuncMinBWAlloc
-	// NumberDiscoveredRemotePorts shall be the
-	// number of ports not on this adapter that this port has discovered.
+	// NumberDiscoveredRemotePorts shall be the number of ports not on this
+	// adapter that this port has discovered.
 	NumberDiscoveredRemotePorts int
-	// Oem is The value of this string shall be of the format for the
-	// reserved word *Oem*.
-	OEM string `json:"Oem"`
-	// PhysicalPortNumber shall be the physical
-	// port number on the network adapter hardware that this Network Port
-	// corresponds to. This value should match a value visible on the
-	// hardware. When HostPortEnabled and ManagementPortEnabled are both
-	// "false", the port shall not establish physical link.
+	// PhysicalPortNumber shall be the physical port number on the network
+	// adapter hardware that this Network Port corresponds to. This value should
+	// match a value visible on the hardware. When HostPortEnabled and
+	// ManagementPortEnabled are both "false", the port shall not establish
+	// physical link.
 	PhysicalPortNumber string
-	// PortMaximumMTU shall be the largest
-	// maximum transmission unit (MTU) that can be configured for this
-	// network port.
+	// PortMaximumMTU shall be the largest maximum transmission unit (MTU) that
+	// can be configured for this network port.
 	PortMaximumMTU int
 	// SignalDetected shall be a boolean
 	// indicating whether the port has detected enough signal on enough lanes
@@ -217,25 +210,6 @@ type NetworkPort struct {
 	// WakeOnLANEnabled shall be a boolean
 	// indicating whether Wake on LAN (WoL) is enabled for this network port.
 	WakeOnLANEnabled bool
-}
-
-// UnmarshalJSON unmarshals a NetworkPort object from the raw JSON.
-func (networkport *NetworkPort) UnmarshalJSON(b []byte) error {
-	type temp NetworkPort
-	var t struct {
-		temp
-	}
-
-	err := json.Unmarshal(b, &t)
-	if err != nil {
-		return err
-	}
-
-	*networkport = NetworkPort(t.temp)
-
-	// Extract the links to other entities for later
-
-	return nil
 }
 
 // GetNetworkPort will get a NetworkPort instance from the service.
@@ -280,18 +254,17 @@ func ListReferencedNetworkPorts(c common.Client, link string) ([]*NetworkPort, e
 	return result, nil
 }
 
-// SupportedLinkCapabilities shall describe the static
-// capabilities of an associated port, irrespective of transient
-// conditions such as cabling, interface module presence, or remote link
-// parter status or configuration.
+// SupportedLinkCapabilities shall describe the static capabilities of an
+// associated port, irrespective of transient conditions such as cabling,
+// interface module presence, or remote link parter status or configuration.
 type SupportedLinkCapabilities struct {
-	// AutoSpeedNegotiation shall be indicate
-	// whether the port is capable of auto-negotiating speed.
+	// AutoSpeedNegotiation shall be indicate whether the port is capable of
+	// auto-negotiating speed.
 	AutoSpeedNegotiation bool
-	// CapableLinkSpeedMbps shall be all of the
-	// possible network link speed capabilities of this port.
-	CapableLinkSpeedMbps []string
-	// LinkNetworkTechnology shall be a network
-	// technology capability of this port.
+	// CapableLinkSpeedMbps shall be all of the possible network link speed
+	// capabilities of this port.
+	CapableLinkSpeedMbps []int
+	// LinkNetworkTechnology shall be a network technology capability of this
+	// port.
 	LinkNetworkTechnology LinkNetworkTechnology
 }
---
+++ school/redfish/networkport_test.go
@@ -0,0 +1,123 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package redfish
+
+import (
+	"encoding/json"
+	"strings"
+	"testing"
+)
+
+var networkPortBody = strings.NewReader(
+	`{
+		"@odata.context": "/redfish/v1/$metadata#NetworkPort.NetworkPort",
+		"@odata.type": "#NetworkPort.v1_2_2.NetworkPort",
+		"@odata.id": "/redfish/v1/NetworkPort",
+		"Id": "NetworkPort-1",
+		"Name": "NetworkPortOne",
+		"Description": "NetworkPort One",
+		"ActiveLinkTechnology": "Ethernet",
+		"AssociatedNetworkAddress": [
+			"98:E7:43:00:01:0A"
+		],
+		"CurrentLinkSpeedMbps": 1000,
+		"EEEEnabled": true,
+		"FlowControlConfiguration": "TX_RX",
+		"FlowControlStatus": "TX_RX",
+		"LinkStatus": "Up",
+		"MaxFrameSize": 900,
+		"NetDevFuncMaxBWAlloc": [{
+				"MaxBWAllocPercent": 100,
+				"NetworkDeviceFunction": {
+					"@odata.id": "/redfish/v1/Function/1"
+				}
+			},
+			{
+				"MaxBWAllocPercent": 100,
+				"NetworkDeviceFunction": {
+					"@odata.id": "/redfish/v1/Function/2"
+				}
+			}
+		],
+		"NetDevFuncMinBWAlloc": [{
+				"MinBWAllocPercent": 25,
+				"NetworkDeviceFunction": {
+					"@odata.id": "/redfish/v1/Function/1"
+				}
+			},
+			{
+				"MinBWAllocPercent": 10,
+				"NetworkDeviceFunction": {
+					"@odata.id": "/redfish/v1/Function/2"
+				}
+			}
+		],
+		"NumberDiscoveredRemotePorts": 42,
+		"PhysicalPortNumber": "10",
+		"PortMaximumMTU": 100,
+		"SignalDetected": true,
+		"Status": {
+			"State": "Enabled",
+			"Health": "OK"
+		},
+		"SupportedEthernetCapabilities": [
+			"WakeOnLAN",
+			"EEE"
+		],
+		"SupportedLinkCapabilities": [{
+			"AutoSpeedNegotiation": true,
+			"CapableLinkSpeedMbps": [1000, 100],
+			"LinkNetworkTechnology": "Ethernet",
+			"LinkSpeedMbps": 1000
+		}],
+		"VendorId": "Vendor-ID",
+		"WakeOnLANEnabled": false
+	}`)
+
+// TestNetworkPort tests the parsing of NetworkPort objects.
+func TestNetworkPort(t *testing.T) {
+	var result NetworkPort
+	err := json.NewDecoder(networkPortBody).Decode(&result)
+
+	if err != nil {
+		t.Errorf("Error decoding JSON: %s", err)
+	}
+
+	if result.ID != "NetworkPort-1" {
+		t.Errorf("Received invalid ID: %s", result.ID)
+	}
+
+	if result.Name != "NetworkPortOne" {
+		t.Errorf("Received invalid name: %s", result.Name)
+	}
+
+	if result.ActiveLinkTechnology != EthernetLinkNetworkTechnology {
+		t.Errorf("Invalid active link technology: %s", result.ActiveLinkTechnology)
+	}
+
+	if result.CurrentLinkSpeedMbps != 1000 {
+		t.Errorf("Invalid current link speed: %d", result.CurrentLinkSpeedMbps)
+	}
+
+	if result.FlowControlConfiguration != TXRXFlowControl {
+		t.Errorf("Invalid flow control config: %s", result.FlowControlConfiguration)
+	}
+
+	if result.LinkStatus != UpPortLinkStatus {
+		t.Errorf("Invalid link status: %s", result.LinkStatus)
+	}
+
+	if !result.SignalDetected {
+		t.Error("Signal detected should be true")
+	}
+}
