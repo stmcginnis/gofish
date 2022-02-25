aad56b20bf824ba0c3149829b15cd9394d3f5316
Leslie Qi Wang <leslie.qiwa@gmail.com>
2022-02-24 18:20:46 -0800

    fix type of prefix length of ipv6 address
    range of prefix length of ipv6 address should be 1-128.
    
    Signed-off-by: Leslie Qi Wang <leslie.qiwa@gmail.com>

--- redfish/ethernetinterface_test.go
+++ redfish/ethernetinterface_test.go
@@ -165,3 +165,76 @@ func TestEthernetInterfaceUpdate(t *testing.T) {
 		t.Errorf("Unexpected update for FullDuplex in payload: %s", calls[0].Payload)
 	}
 }
+
+var ethernetInterfaceIPv6Body = `{
+		"@odata.context": "/redfish/v1/$metadata#EthernetInterface.EthernetInterface",
+		"@odata.id": "/redfish/v1/Systems/System-1/EthernetInterfaces/NIC-0",
+		"@odata.type": "#EthernetInterface.v1_3_0.EthernetInterface",
+		"IPv6Addresses": [
+			{
+				"Address": "FE80::B67A:F1FF:FECF:6462",
+				"AddressOrigin": "SLAAC",
+				"AddressState": "Preferred",
+				"PrefixLength": 64
+			},
+			{
+				"Address": "FDE1:53BA:E9A0:DE41::1649",
+				"AddressOrigin": "DHCP",
+				"AddressState": "Preferred",
+				"PrefixLength": 128
+			}
+		],
+		"Id": "NIC-0",
+		"InterfaceEnabled": true,
+		"LinkStatus": "LinkUp",
+		"Links": {
+			"Chassis": {
+				"@odata.id": "/redfish/v1/Chassis/Chassis-1"
+			}
+		},
+		"MACAddress": "f6:a9:26:e3:e6:32",
+		"MTUSize": 1500,
+		"Name": "Ethernet Interface",
+		"NameServers": [
+			"8.8.8.8"
+		],
+		"PermanentMACAddress": "f6:a9:26:e3:e6:32",
+		"SpeedMbps": 10000,
+		"Status": {
+			"Health": "OK",
+			"State": "Enabled"
+		},
+		"VLAN": {
+			"VLANId": 0
+		}
+	}`
+
+// TestEthernetInterface tests the parsing of EthernetInterface objects.
+func TestEthernetInterfaceIPv6(t *testing.T) {
+	var result EthernetInterface
+	err := json.NewDecoder(strings.NewReader(ethernetInterfaceIPv6Body)).Decode(&result)
+
+	if err != nil {
+		t.Errorf("Error decoding JSON: %s", err)
+	}
+
+	if result.ID != "NIC-0" {
+		t.Errorf("Received invalid ID: %s", result.ID)
+	}
+
+	if result.Name != "Ethernet Interface" {
+		t.Errorf("Received invalid name: %s", result.Name)
+	}
+
+	if len(result.IPv6Addresses) != 2 {
+		t.Errorf("Should be 2 IPv6 addresses, got: %d", len(result.IPv4Addresses))
+	}
+
+	if result.IPv6Addresses[0].PrefixLength != 64 {
+		t.Errorf("The 1st IPv6 address's prefix length should be 64, got: %d", result.IPv6Addresses[0].PrefixLength)
+	}
+
+	if result.IPv6Addresses[1].PrefixLength != 128 {
+		t.Errorf("The 3nd IPv6 address's prefix length should be 128, got: %d", result.IPv6Addresses[1].PrefixLength)
+	}
+}
