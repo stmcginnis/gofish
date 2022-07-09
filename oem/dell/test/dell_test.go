d3e2dde6b4cc1df1a282c0af713d39e37cbd8e6f
Niv Mordechai Gal Waizer <83756183+nwaizer@users.noreply.github.com>
2022-07-10 01:40:33 +0300

    Add OEM specific handling for EventService (#187)

---
+++ oem/dell/test/dell_test.go
@@ -0,0 +1,237 @@
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//
+
+package test
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stmcginnis/gofish"
+	"github.com/stmcginnis/gofish/common"
+	"github.com/stmcginnis/gofish/oem/dell"
+	"github.com/stmcginnis/gofish/redfish"
+)
+
+const serviceRootBody = `{
+    "@odata.context": "/redfish/v1/$metadata#ServiceRoot.ServiceRoot",
+    "@odata.id": "/redfish/v1",
+    "@odata.type": "#ServiceRoot.v1_8_0.ServiceRoot",
+    "AccountService": {
+        "@odata.id": "/redfish/v1/AccountService"
+    },
+    "CertificateService": {
+        "@odata.id": "/redfish/v1/CertificateService"
+    },
+    "Chassis": {
+        "@odata.id": "/redfish/v1/Chassis"
+    },
+    "Description": "Root Service",
+    "EventService": {
+        "@odata.id": "/redfish/v1/EventService"
+    },
+    "Fabrics": {
+        "@odata.id": "/redfish/v1/Fabrics"
+    },
+    "Id": "RootService",
+    "JobService": {
+        "@odata.id": "/redfish/v1/JobService"
+    },
+    "JsonSchemas": {
+        "@odata.id": "/redfish/v1/JsonSchemas"
+    },
+    "Links": {
+        "Sessions": {
+            "@odata.id": "/redfish/v1/SessionService/Sessions"
+        }
+    },
+    "Managers": {
+        "@odata.id": "/redfish/v1/Managers"
+    },
+    "Name": "Root Service",
+    "Oem": {
+        "Dell": {
+            "@odata.context": "/redfish/v1/$metadata#DellServiceRoot.DellServiceRoot",
+            "@odata.type": "#DellServiceRoot.v1_0_0.DellServiceRoot",
+            "IsBranded": 0,
+            "ManagerMACAddress": "00:00:00:00:00:00",
+            "ServiceTag": "0000000"
+        }
+    },
+    "Product": "Integrated Dell Remote Access Controller",
+    "ProtocolFeaturesSupported": {
+        "DeepOperations": {
+            "DeepPATCH": false,
+            "DeepPOST": false
+        },
+        "ExcerptQuery": false,
+        "ExpandQuery": {
+            "ExpandAll": true,
+            "Levels": true,
+            "Links": true,
+            "MaxLevels": 1,
+            "NoLinks": true
+        },
+        "FilterQuery": true,
+        "OnlyMemberQuery": true,
+        "SelectQuery": true
+    },
+    "RedfishVersion": "1.11.0",
+    "Registries": {
+        "@odata.id": "/redfish/v1/Registries"
+    },
+    "SessionService": {
+        "@odata.id": "/redfish/v1/SessionService"
+    },
+    "Systems": {
+        "@odata.id": "/redfish/v1/Systems"
+    },
+    "Tasks": {
+        "@odata.id": "/redfish/v1/TaskService"
+    },
+    "TelemetryService": {
+        "@odata.id": "/redfish/v1/TelemetryService"
+    },
+    "UpdateService": {
+        "@odata.id": "/redfish/v1/UpdateService"
+    },
+    "Vendor": "Dell"
+}`
+
+const eventServiceBody = `{
+    "@odata.context": "/redfish/v1/$metadata#EventService.EventService",
+    "@odata.id": "/redfish/v1/EventService",
+    "@odata.type": "#EventService.v1_7_0.EventService",
+    "Actions": {
+        "#EventService.SubmitTestEvent": {
+            "EventType@Redfish.AllowableValues": ["Alert"],
+            "target": "/redfish/v1/EventService/Actions/EventService.SubmitTestEvent"
+        }
+    },
+    "DeliveryRetryAttempts": 3,
+    "DeliveryRetryIntervalSeconds": 5,
+    "Description": "Event Service represents the properties for the service",
+    "EventFormatTypes": ["Event", "MetricReport"],
+    "EventFormatTypes@odata.count": 2,
+    "EventTypesForSubscription": ["Alert", "MetricReport", "Other"],
+    "EventTypesForSubscription@odata.count": 3,
+    "Id": "EventService",
+    "Name": "Event Service",
+    "SMTP": {
+        "Authentication": "None",
+        "ConnectionProtocol": "StartTLS",
+        "FromAddress": "",
+        "Password": null,
+        "Port": 25,
+        "ServerAddress": "0.0.0.0",
+        "Username": ""
+    },
+    "SSEFilterPropertiesSupported": {
+        "EventFormatType": true,
+        "EventType": true,
+        "MessageId": true,
+        "MetricReportDefinition": true,
+        "OriginResource": true,
+        "RegistryPrefix": true,
+        "ResourceType": true,
+        "SubordinateResources": false
+    },
+    "ServerSentEventUri": "/redfish/v1/SSE",
+    "ServiceEnabled": true,
+    "Status": {
+        "Health": "OK",
+        "HealthRollup": "OK",
+        "State": "Enabled"
+    },
+    "Subscriptions": {
+        "@odata.id": "/redfish/v1/EventService/Subscriptions"
+    }
+}`
+
+func TestDellSubmitTestEvent(t *testing.T) {
+	const redfishBaseURL = "/redfish/v1/"
+	var (
+		c              common.Client
+		err            error
+		requestCounter int // this counter is used to verify that the received requests, are in the expected order
+	)
+
+	// Start a local HTTP server
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		if req.Method == http.MethodGet &&
+			req.URL.String() == redfishBaseURL &&
+			requestCounter < 2 { // ServiceRoot
+			log.Printf("Mock received login request")
+			contentType := req.Header.Get("Content-Type")
+			if contentType != "application/json" {
+				t.Errorf("gofish connect sent wrong header. Content-Type:"+
+					" is %v and not expected application/json", contentType)
+			}
+
+			requestCounter++
+
+			// Send response to be tested
+			rw.WriteHeader(http.StatusOK)
+			rw.Header().Set("Content-Type", "application/json")
+
+			rw.Write([]byte(serviceRootBody)) // nolint:errcheck
+		} else if req.Method == http.MethodGet && // Get event service
+			req.URL.String() == "/redfish/v1/EventService" &&
+			requestCounter == 2 {
+			log.Printf("Getting event service")
+
+			requestCounter++
+
+			rw.Write([]byte(eventServiceBody)) // nolint:errcheck
+		} else if req.Method == http.MethodPost && // SubmitTestEvent
+			req.URL.String() == dell.SubmitTestEventTarget &&
+			requestCounter == 3 {
+			log.Printf("Mock got SubmitTestEvent POST")
+
+			err := json.NewDecoder(req.Body).Decode(&dell.PayloadType{})
+			if err != nil {
+				t.Errorf("error in SubmitTestEvent payload for Dell due to: %v", err)
+			}
+
+			requestCounter++
+
+			rw.WriteHeader(http.StatusCreated)
+		} else {
+			t.Errorf("mock got unexpected %v request to path %v while request counter is %v",
+				req.Method, req.URL.String(), requestCounter)
+		}
+	}))
+	// Close the server when test finishes
+	defer server.Close()
+
+	c, err = gofish.Connect(gofish.ClientConfig{Endpoint: server.URL, HTTPClient: server.Client()})
+
+	if err != nil {
+		t.Errorf("failed to establish client to mock http server due to: %v", err)
+	}
+
+	serviceRoot, err := gofish.ServiceRoot(c)
+	if err != nil {
+		t.Errorf("failed to get redfish service root due to: %v", err)
+	}
+	eventService, err := serviceRoot.EventService()
+	if err != nil {
+		t.Errorf("failed to get event service due to: %v", err)
+	}
+	dellEventService, err := dell.FromEventService(eventService)
+	if err != nil {
+		t.Errorf("failed to get dell event service due to: %v", err)
+	}
+
+	err = dellEventService.SubmitTestEvent(
+		"AMP0300",
+		"Alert",
+		string(redfish.RedfishEventDestinationProtocol))
+	if err != nil {
+		t.Errorf("failed to submit test event due to: %v", err)
+	}
+}
---
+++ oem/zt/test/zt_test.go
@@ -0,0 +1,243 @@
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//
+
+package test
+
+import (
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stmcginnis/gofish"
+	"github.com/stmcginnis/gofish/common"
+	"github.com/stmcginnis/gofish/oem/zt"
+	"github.com/stmcginnis/gofish/redfish"
+)
+
+const serviceRootBody = `{
+"@odata.context": "/redfish/v1/$metadata#ServiceRoot.ServiceRoot",
+"@odata.etag": "\"0000000000\"",
+"@odata.id": "/redfish/v1/",
+"@odata.type": "#ServiceRoot.v1_5_2.ServiceRoot",
+"AccountService": {
+"@odata.id": "/redfish/v1/AccountService"
+},
+"CertificateService": {
+"@odata.id": "/redfish/v1/CertificateService"
+},
+"Chassis": {
+"@odata.id": "/redfish/v1/Chassis"
+},
+"CompositionService": {
+"@odata.id": "/redfish/v1/CompositionService"
+},
+"Description": "The service root for all Redfish requests on this host",
+"EventService": {
+"@odata.id": "/redfish/v1/EventService"
+},
+"Id": "RootService",
+"JsonSchemas": {
+"@odata.id": "/redfish/v1/JsonSchemas"
+},
+"Links": {
+"Sessions": {
+"@odata.id": "/redfish/v1/SessionService/Sessions"
+}
+},
+"Managers": {
+"@odata.id": "/redfish/v1/Managers"
+},
+"Name": "Root Service",
+"Oem": {
+"Ami": {
+"@odata.type": "#AMIServiceRoot.v1_0_0.AMIServiceRoot",
+"InventoryDataStatus": {
+"@odata.id": "/redfish/v1/Oem/Ami/InventoryData/Status"
+},
+"RtpVersion": "1.8.a",
+"configurations": {
+"@odata.id": "/redfish/v1/configurations"
+}
+}
+},
+"Product": "AMI Redfish Server",
+"ProtocolFeaturesSupported": {
+"ExcerptQuery": true,
+"ExpandQuery": {
+"ExpandAll": true,
+"Levels": true,
+"Links": true,
+"MaxLevels": 5,
+"NoLinks": true
+},
+"FilterQuery": true,
+"OnlyMemberQuery": true,
+"SelectQuery": true
+},
+"RedfishVersion": "1.8.0",
+"Registries": {
+"@odata.id": "/redfish/v1/Registries"
+},
+"SessionService": {
+"@odata.id": "/redfish/v1/SessionService"
+},
+"Systems": {
+"@odata.id": "/redfish/v1/Systems"
+},
+"Tasks": {
+"@odata.id": "/redfish/v1/TaskService"
+},
+"TelemetryService": {
+"@odata.id": "/redfish/v1/TelemetryService"
+},
+"UUID": "00000000-0000-0000-0000-000000000000",
+"UpdateService": {
+"@odata.id": "/redfish/v1/UpdateService"
+},
+"Vendor": "AMI"
+}`
+
+const eventServiceBody = `{
+    "@odata.context": "/redfish/v1/$metadata#EventService.EventService",
+    "@odata.etag": "\"0000000000\"",
+    "@odata.id": "/redfish/v1/EventService",
+    "@odata.type": "#EventService.v1_5_0.EventService",
+    "Actions": {
+        "#EventService.SubmitTestEvent": {
+            "@Redfish.ActionInfo": "/redfish/v1/EventService/SubmitTestEventActionInfo",
+            "target": "/redfish/v1/EventService/Actions/EventService.SubmitTestEvent"
+        }
+    },
+    "DeliveryRetryAttempts": 3,
+    "DeliveryRetryIntervalSeconds": 60,
+    "Description": "Event Service",
+    "EventFormatTypes": ["MetricReport", "Event"],
+    "Id": "EventService",
+    "Name": "Event Service",
+    "RegistryPrefixes": ["SyncAgent", "Security", "IPMI", "EventLog", "Task", "HttpStatus", "Base"],
+    "ResourceTypes": ["Systems", "Managers", "EventService", "TelemetryService", "AccountService", "TaskService", "Chassis"],
+    "SSEFilterPropertiesSupported": {
+        "EventFormatType": true,
+        "MessageId": true,
+        "MetricReportDefinition": false,
+        "OriginResource": true,
+        "RegistryPrefix": true,
+        "ResourceType": true,
+        "SubordinateResources": false
+    },
+    "ServerSentEventUri": "/redfish/v1/EventService/SSE",
+    "ServiceEnabled": true,
+    "Status": {
+        "Health": "OK",
+        "State": "Enabled"
+    },
+    "SubordinateResourcesSupported": false,
+    "Subscriptions": {
+        "@odata.id": "/redfish/v1/EventService/Subscriptions"
+    }
+}`
+
+const subscribeResponseBody = `{
+    "@odata.context": "/redfish/v1/$metadata#EventDestination.EventDestination",
+    "@odata.etag": "\"0000000000\"",
+    "@odata.id": "/redfish/v1/EventService/Subscriptions",
+    "@odata.type": "#EventDestination.v1_6_0.EventDestination",
+    "Context": "root",
+    "DeliveryRetryPolicy": "TerminateAfterRetries",
+    "Description": "Event Subscription",
+    "Destination": "https://events.receiver/events/",
+    "EventFormatType": "Event",
+    "Id": 1,
+    "Name": "Subscription 1",
+    "Protocol": "Redfish",
+    "Status": {
+        "Health": "OK",
+        "HealthRollup": "OK",
+        "State": "Enabled"
+    },
+    "SubordinateResources": false,
+    "SubscriptionType": "RedfishEvent"
+}`
+
+func TestSubscribeZT(t *testing.T) {
+	const redfishBaseURL = "/redfish/v1/"
+
+	var (
+		c              common.Client
+		requestCounter int // this counter is used to verify that the received requests, are in the expected order
+		err            error
+	)
+
+	// Start a local HTTP server
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		if req.Method == http.MethodGet &&
+			req.URL.String() == redfishBaseURL &&
+			requestCounter < 2 { // ServiceRoot
+			log.Printf("Mock received login request")
+			contentType := req.Header.Get("Content-Type")
+			if contentType != "application/json" {
+				t.Errorf("gofish connect sent wrong header. Content-Type:"+
+					" is %v and not expected application/json", contentType)
+			}
+
+			requestCounter++
+			// Send response to be tested
+			rw.WriteHeader(http.StatusOK)
+			rw.Header().Set("Content-Type", "application/json")
+
+			rw.Write([]byte(serviceRootBody)) // nolint:errcheck
+		} else if req.Method == http.MethodGet && // Get event service
+			req.URL.String() == "/redfish/v1/EventService" &&
+			requestCounter == 2 {
+			log.Printf("Getting event service")
+
+			requestCounter++
+
+			rw.Write([]byte(eventServiceBody)) // nolint:errcheck
+		} else if req.Method == http.MethodPost && // Subscribe
+			req.URL.String() == zt.SubscriptionURL &&
+			requestCounter == 3 {
+			log.Printf("Mock got suscription POST")
+
+			requestCounter++
+
+			rw.Write([]byte(subscribeResponseBody)) // nolint:errcheck
+		} else {
+			t.Errorf("mock got unexpected %v request to path %v while request counter is %v",
+				req.Method, req.URL.String(), requestCounter)
+		}
+	}))
+	// Close the server when test finishes
+	defer server.Close()
+
+	c, err = gofish.Connect(gofish.ClientConfig{Endpoint: server.URL, HTTPClient: server.Client()})
+	if err != nil {
+		t.Errorf("failed to establish redfish session due to: %v", err)
+	}
+
+	serviceRoot, err := gofish.ServiceRoot(c)
+	if err != nil {
+		t.Errorf("failed to get redfish service root due to: %v", err)
+	}
+	eventService, err := serviceRoot.EventService()
+	if err != nil {
+		t.Errorf("failed to get event service due to: %v", err)
+	}
+	ztEventService, err := zt.FromEventService(eventService)
+	if err != nil {
+		t.Errorf("failed to get zt systems event service due to: %v", err)
+	}
+	url, err := ztEventService.Subscribe(
+		"https://events.receiver/events/",
+		string(redfish.RedfishEventDestinationProtocol))
+	if err != nil {
+		t.Errorf("failed to Subscribe() due to: %v", err)
+	}
+
+	expectedSubURL := "/redfish/v1/EventService/Subscriptions/1"
+	if url != expectedSubURL {
+		t.Errorf("expected subscription url: %v got %v", expectedSubURL, url)
+	}
+}
