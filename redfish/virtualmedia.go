c4c5d36c7df9bfac3239a754ecac8df56a11ba08
Markus Witt <feuerrot@feuerrot.org>
2022-10-25 20:05:43 +0200

    Fix VirtualMediaConfig for Supermicro (#212)
    The redfish implementation of Supermicro doesn't like "Inserted" or
    "WriteProtected" as additional properties for the InsertMedia action and
    rejects requests which include them.

--- redfish/virtualmedia.go
+++ redfish/virtualmedia.go
@@ -236,12 +236,12 @@ func (virtualmedia *VirtualMedia) InsertMedia(image string, inserted, writeProte
 // VirtualMediaConfig is an struct used to pass config data to build the HTTP body when inserting media
 type VirtualMediaConfig struct {
 	Image                string
-	Inserted             bool
+	Inserted             bool   `json:",omitempty"`
 	Password             string `json:",omitempty"`
 	TransferMethod       string `json:",omitempty"`
 	TransferProtocolType string `json:",omitempty"`
 	UserName             string `json:",omitempty"`
-	WriteProtected       bool
+	WriteProtected       bool   `json:",omitempty"`
 }
 
 // InsertMediaConfig sends a request to insert virtual media using the VirtualMediaConfig struct
