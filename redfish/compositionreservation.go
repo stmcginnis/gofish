aa628d30531c2a3f2d2ac59792df3b8fd402ae45
Sean McGinnis <sean.mcginnis@gmail.com>
2024-04-10 16:25:46 -0500

    Add CompositionReservation objects
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>

---
+++ redfish/compositionreservation.go
@@ -0,0 +1,146 @@
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//
+
+package redfish
+
+import (
+	"encoding/json"
+
+	"github.com/stmcginnis/gofish/common"
+)
+
+// CompositionReservation This resource represents the composition reservation of the composition service for a
+// Redfish implementation.
+type CompositionReservation struct {
+	common.Entity
+	// ODataContext is the odata context.
+	ODataContext string `json:"@odata.context"`
+	// ODataEtag is the odata etag.
+	ODataEtag string `json:"@odata.etag"`
+	// ODataType is the odata type.
+	ODataType string `json:"@odata.type"`
+	// Client shall contain the client that owns the reservation. The service shall determine this value based on the
+	// client that invoked the Compose action that resulted in the creation of this reservation.
+	Client string
+	// Description provides a description of this resource.
+	Description string
+	// Manifest shall contain the manifest document processed by the service that resulted in this reservation. This
+	// property shall be required if the RequestFormat parameter in the Compose action request contained the value
+	// 'Manifest'.
+	Manifest Manifest
+	// ReservationTime shall indicate the date and time when the reservation was created by the service.
+	ReservationTime string
+	// ReservedResourceBlocks shall contain an array of links to resources of type ResourceBlock that represent the
+	// reserved resource blocks for this reservation. Upon deletion of the reservation or when the reservation is
+	// applied, the Reserved property in the referenced resource blocks shall change to 'false'.
+	reservedResourceBlocks []string
+	// ReservedResourceBlocksCount is the number of ResourceBlocks for this reservation.
+	ReservedResourceBlocksCount int `json:"ReservedResourceBlocks@odata.count"`
+}
+
+// UnmarshalJSON unmarshals a CompositionReservation object from the raw JSON.
+func (compositionreservation *CompositionReservation) UnmarshalJSON(b []byte) error {
+	type temp CompositionReservation
+	var t struct {
+		temp
+		ReservedResourceBlocks common.Links
+	}
+
+	err := json.Unmarshal(b, &t)
+	if err != nil {
+		return err
+	}
+
+	*compositionreservation = CompositionReservation(t.temp)
+
+	// Extract the links to other entities for later
+	compositionreservation.reservedResourceBlocks = t.ReservedResourceBlocks.ToStrings()
+
+	return nil
+}
+
+// GetCompositionReservation will get a CompositionReservation instance from the service.
+func GetCompositionReservation(c common.Client, uri string) (*CompositionReservation, error) {
+	resp, err := c.Get(uri)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var compositionreservation CompositionReservation
+	err = json.NewDecoder(resp.Body).Decode(&compositionreservation)
+	if err != nil {
+		return nil, err
+	}
+
+	compositionreservation.SetClient(c)
+	return &compositionreservation, nil
+}
+
+// ListReferencedCompositionReservations gets the collection of CompositionReservation from
+// a provided reference.
+func ListReferencedCompositionReservations(c common.Client, link string) ([]*CompositionReservation, error) {
+	var result []*CompositionReservation
+	if link == "" {
+		return result, nil
+	}
+
+	type GetResult struct {
+		Item  *CompositionReservation
+		Link  string
+		Error error
+	}
+
+	ch := make(chan GetResult)
+	collectionError := common.NewCollectionError()
+	get := func(link string) {
+		compositionreservation, err := GetCompositionReservation(c, link)
+		ch <- GetResult{Item: compositionreservation, Link: link, Error: err}
+	}
+
+	go func() {
+		err := common.CollectList(get, c, link)
+		if err != nil {
+			collectionError.Failures[link] = err
+		}
+		close(ch)
+	}()
+
+	for r := range ch {
+		if r.Error != nil {
+			collectionError.Failures[r.Link] = r.Error
+		} else {
+			result = append(result, r.Item)
+		}
+	}
+
+	if collectionError.Empty() {
+		return result, nil
+	}
+
+	return result, collectionError
+}
+
+// ReservedResourceBlocks gets reserved resource blocks for this reservation.
+// Upon deletion of the reservation or when the reservation is applied, the
+// Reserved property in the referenced resource blocks shall change to 'false'.
+func (compositionreservation *CompositionReservation) ReservedResourceBlocks() ([]*ResourceBlock, error) {
+	var result []*ResourceBlock
+
+	collectionError := common.NewCollectionError()
+	for _, uri := range compositionreservation.reservedResourceBlocks {
+		rb, err := GetResourceBlock(compositionreservation.GetClient(), uri)
+		if err != nil {
+			collectionError.Failures[uri] = err
+		} else {
+			result = append(result, rb)
+		}
+	}
+
+	if collectionError.Empty() {
+		return result, nil
+	}
+
+	return result, collectionError
+}
