d438f8a69f63d01c522c4423629b4a2b42b5651a
Sean McGinnis <sean.mcginnis@gmail.com>
2024-04-15 13:18:59 -0500

    Update MessageRegistryFile object
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>

--- redfish/messageregistryfile.go
+++ redfish/messageregistryfile.go
@@ -5,14 +5,32 @@
 package redfish
 
 import (
+	"encoding/json"
+
 	"github.com/stmcginnis/gofish/common"
 )
 
 // MessageRegistryFileLocation is a location
 // information for the Message Registry file.
 type MessageRegistryFileLocation struct {
-	Language string `json:"Language"`
-	URI      string `json:"Uri"`
+	// ArchiveFile shall contain the file name of the individual registry file within the archive file specified by the
+	// ArchiveUri property. The file name shall conform to the Redfish Specification-specified syntax.
+	ArchiveFile string
+	// ArchiveURI shall contain a URI that is colocated with the Redfish service that specifies the location of the
+	// registry file, which can be retrieved using the Redfish protocol and authentication methods. This property shall
+	// be used for only ZIP or other archive files. The ArchiveFile property shall contain the file name of the
+	// individual registry file within the archive file.
+	ArchiveURI string
+	// Language shall contain an RFC5646-conformant language code or 'default'.
+	Language string
+	// PublicationURI shall contain a URI not colocated with the Redfish service that specifies the canonical location
+	// of the registry file. This property shall be used for only individual registry files.
+	PublicationURI string
+	// Uri shall contain a URI colocated with the Redfish service that specifies the location of the registry file,
+	// which can be retrieved using the Redfish protocol and authentication methods. This property shall be used for
+	// only individual registry files. The file name portion of the URI shall conform to Redfish Specification-
+	// specified syntax.
+	URI string
 }
 
 // MessageRegistryFile describes the Message Registry file locator Resource.
@@ -21,6 +39,8 @@ type MessageRegistryFile struct {
 
 	// ODataContext is the odata context.
 	ODataContext string `json:"@odata.context"`
+	// ODataEtag is the odata etag.
+	ODataEtag string `json:"@odata.etag"`
 	// ODataType is the odata type.
 	ODataType string `json:"@odata.type"`
 	// Description provides a description of this resource.
@@ -30,6 +50,9 @@ type MessageRegistryFile struct {
 	Languages []string
 	// Location is the location information for this Message Registry file.
 	Location []MessageRegistryFileLocation
+	// Oem shall contain the OEM extensions. All values for properties that this object contains shall conform to the
+	// Redfish Specification-described requirements.
+	OEM json.RawMessage `json:"Oem"`
 	// Registry shall contain the Message Registry name and it major and
 	// minor versions, as defined by the Redfish Specification.
 	Registry string
@@ -37,10 +60,7 @@ type MessageRegistryFile struct {
 
 // GetMessageRegistryFile will get a MessageRegistryFile
 // instance from the Redfish service.
-func GetMessageRegistryFile(
-	c common.Client,
-	uri string,
-) (*MessageRegistryFile, error) {
+func GetMessageRegistryFile(c common.Client, uri string) (*MessageRegistryFile, error) {
 	var messageRegistryFile MessageRegistryFile
 	return &messageRegistryFile, messageRegistryFile.Get(c, uri, &messageRegistryFile)
 }
