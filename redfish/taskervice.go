48205c697feaa6d9f295a82bc89c32caf71dff12
Sean McGinnis <sean.mcginnis@gmail.com>
2024-04-17 11:11:33 -0500

    Update TaskService object
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>

--- redfish/taskervice.go
+++ redfish/taskervice.go
@@ -6,9 +6,19 @@ package redfish
 
 import (
 	"encoding/json"
+	"reflect"
 	"time"
 
 	"github.com/stmcginnis/gofish/common"
+)
+
+type TaskOverWritePolicy string
+
+const (
+	// ManualTaskOverWritePolicy Completed tasks are not automatically overwritten.
+	ManualTaskOverWritePolicy TaskOverWritePolicy = "Manual"
+	// OldestTaskOverWritePolicy Oldest completed tasks are overwritten.
+	OldestTaskOverWritePolicy TaskOverWritePolicy = "Oldest"
 )
 
 // TaskService is used to represent the task service offered by the redfish API
@@ -22,17 +32,25 @@ type TaskService struct {
 	// Description provides a description of this resource.
 	Description string
 	// CompletedTaskOverWritePolicy how to handle the completed tasks.
-	CompletedTaskOverWritePolicy string
+	CompletedTaskOverWritePolicy TaskOverWritePolicy
 	// DateTime system time.
 	DateTime time.Time
 	// LifeCycleEventOnTaskStateChange whether an event is reported when the task status is changed.
 	LifeCycleEventOnTaskStateChange bool
+	// Oem shall contain the OEM extensions. All values for properties that this object contains shall conform to the
+	// Redfish Specification-described requirements.
+	OEM json.RawMessage `json:"Oem"`
 	// ServiceEnabled indicates whether this service isenabled.
 	ServiceEnabled bool
 	// Status describes the status and health of a resource and its children.
 	Status common.Status
+	// TaskAutoDeleteTimeoutMinutes shall contain the number of minutes after which a completed task, where TaskState
+	// contains the value 'Completed', 'Killed', 'Cancelled', or 'Exception', is deleted by the service.
+	TaskAutoDeleteTimeoutMinutes int
 	// tasks points towards the tasks store endpoint
 	tasks string
+	// rawData holds the original serialized JSON so we can compare updates.
+	rawData []byte
 }
 
 // UnmarshalJSON unmarshals a TaskService object from the raw JSON.
@@ -52,16 +70,37 @@ func (taskService *TaskService) UnmarshalJSON(b []byte) error {
 	*taskService = TaskService(t.temp)
 	taskService.tasks = t.Tasks.String()
 
+	// This is a read/write object, so we need to save the raw object data for later
+	taskService.rawData = b
+
 	return nil
+}
+
+// Tasks gets the collection of tasks of this task service
+func (taskService *TaskService) Tasks() ([]*Task, error) {
+	return ListReferencedTasks(taskService.GetClient(), taskService.tasks)
+}
+
+// Update commits updates to this object's properties to the running system.
+func (taskService *TaskService) Update() error {
+	// Get a representation of the object's original state so we can find what
+	// to update.
+	original := new(TaskService)
+	original.UnmarshalJSON(taskService.rawData)
+
+	readWriteFields := []string{
+		"ServiceEnabled",
+		"TaskAutoDeleteTimeoutMinutes",
+	}
+
+	originalElement := reflect.ValueOf(original).Elem()
+	currentElement := reflect.ValueOf(taskService).Elem()
+
+	return taskService.Entity.Update(originalElement, currentElement, readWriteFields)
 }
 
 // GetTaskService will get a TaskService instance from the service.
 func GetTaskService(c common.Client, uri string) (*TaskService, error) {
 	var taskService TaskService
 	return &taskService, taskService.Get(c, uri, &taskService)
-}
-
-// Tasks gets the collection of tasks of this task service
-func (taskService *TaskService) Tasks() ([]*Task, error) {
-	return ListReferencedTasks(taskService.GetClient(), taskService.tasks)
 }
