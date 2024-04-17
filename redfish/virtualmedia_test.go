f4b399954e615db4ead1beeb4e6139a6d039edde
Sean McGinnis <sean.mcginnis@gmail.com>
2024-04-17 13:39:59 -0500

    Update VirtualMedia object
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>

--- redfish/virtualmedia_test.go
+++ redfish/virtualmedia_test.go
@@ -31,7 +31,6 @@ var vmBody = `{
 	"Image": "https://example.com/mygoldimage.iso",
 	"ImageName": "mygoldimage.iso",
 	"Inserted": false,
-	"MediaType": "USBStick",
 	"MediaTypes": [
 	  "Floppy",
 	  "USBStick"
@@ -97,9 +96,6 @@ func TestVirtualMedia(t *testing.T) {
 
 	if result.SupportsMediaInsert == false {
 		t.Error("Expected SupportsMediaInsert to be true since target is set")
-	}
-	if result.MediaType != "USBStick" {
-		t.Errorf("Received invalid MediaType: %s", result.MediaType)
 	}
 }
 
