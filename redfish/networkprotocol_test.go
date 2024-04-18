843f7a717d650c688176ebacc7e76b2f6cea6672
Sean McGinnis <sean.mcginnis@gmail.com>
2024-04-18 11:13:52 -0500

    Update NetworkProtocol tests
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>

--- redfish/networkprotocol_test.go
+++ redfish/networkprotocol_test.go
@@ -127,6 +127,75 @@ var networkProtocolBody = `{
     }
 }`
 
+var managerNetworkProtocolBody = `{
+    "@odata.type": "#ManagerNetworkProtocol.v1_5_0.ManagerNetworkProtocol",
+    "@odata.id": "/redfish/v1/Managers/1/NetworkProtocol",
+    "Id": "NetworkProtocol",
+    "Name": "Manager Network Protocol",
+    "Description": "Manager Network Service Status",
+    "Status": {
+      "State": "Enabled",
+      "Health": "OK"
+    },
+    "HostName": "",
+    "FQDN": "",
+    "HTTP": {
+      "ProtocolEnabled": true,
+      "Port": 80
+    },
+    "HTTPS": {
+      "ProtocolEnabled": true,
+      "Port": 443,
+      "Certificates": {
+        "@odata.id": "/redfish/v1/Managers/1/NetworkProtocol/HTTPS/Certificates"
+      }
+    },
+    "SNMP": {
+      "AuthenticationProtocol": null,
+      "CommunityAccessMode": null,
+      "CommunityStrings": [],
+      "EnableSNMPv2c": false,
+      "EnableSNMPv3": false,
+      "EncryptionProtocol": null,
+      "EngineId": {
+        "PrivateEnterpriseId": "00005345"
+      },
+      "HideCommunityStrings": null,
+      "ProtocolEnabled": false,
+      "Port": 161
+    },
+    "IPMI": {
+      "ProtocolEnabled": true,
+      "Port": 623
+    },
+    "SSH": {
+      "ProtocolEnabled": true,
+      "Port": 22
+    },
+    "VirtualMedia": {
+      "ProtocolEnabled": true,
+      "Port": 623
+    },
+    "KVMIP": {
+      "ProtocolEnabled": true,
+      "Port": 5900
+    },
+    "NTP": {
+      "ProtocolEnabled": false,
+      "NTPServers": [
+        "localhost",
+        "127.0.0.1"
+      ],
+      "NTPServers@odata.count": 2
+    },
+    "SSDP": {
+      "ProtocolEnabled": true,
+      "Port": 1900,
+      "NotifyTTL": 2,
+      "NotifyIPv6Scope": "Link"
+    }
+}`
+
 func TestNetworkProtocol(t *testing.T) {
 	var result NetworkProtocolSettings
 	err := json.NewDecoder(strings.NewReader(networkProtocolBody)).Decode(&result)
@@ -147,6 +216,35 @@ func TestNetworkProtocol(t *testing.T) {
 	}
 	if result.rawData == nil {
 		t.Errorf("rawData shouldn't be nil")
+	}
+}
+
+func TestNetworkProtocolManager(t *testing.T) {
+	var result NetworkProtocolSettings
+	err := json.NewDecoder(strings.NewReader(managerNetworkProtocolBody)).Decode(&result)
+
+	if err != nil {
+		t.Errorf("Error decoding JSON: %s", err)
+	}
+
+	if !result.SSH.ProtocolEnabled {
+		t.Error("SSH Protocol should be enabled")
+	}
+
+	if len(result.NTP.NTPServers) != 2 {
+		t.Error("Should be 2 NTP servers defined")
+	}
+
+	if result.NTP.NTPServers[1] != "127.0.0.1" {
+		t.Error("NTPServers should be set")
+	}
+
+	if result.SNMP.EngineID.PrivateEnterpriseID != "00005345" {
+		t.Error("Wrong SNMP EngineID PrivateEnterpriseId property")
+	}
+
+	if result.HTTPS.certificates != "/redfish/v1/Managers/1/NetworkProtocol/HTTPS/Certificates" {
+		t.Errorf("Wrong HTTPS certificates link: %s", result.HTTPS.certificates)
 	}
 }
 
