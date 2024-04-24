d01755405f22da259b41f8314a0dde3271cf17ca
Sean McGinnis <sean.mcginnis@gmail.com>
2024-04-24 13:48:58 -0500

    Add KeyService test coverage
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>

--- redfish/keyservice.go
+++ redfish/keyservice.go
@@ -24,11 +24,11 @@ type KeyService struct {
 	// NVMeoFKeyPolicies shall contain a link to a resource collection of type KeyPolicyCollection that contains the
 	// NVMe-oF key policies maintained by this service. The KeyPolicyType property for all members of this collection
 	// shall contain the value 'NVMeoF'.
-	nvmeoFKeyPolicies []string
+	nvmeoFKeyPolicies string
 	// NVMeoFSecrets shall contain a link to a resource collection of type KeyCollection that contains the NVMe-oF keys
 	// maintained by this service. The KeyType property for all members of this collection shall contain the value
 	// 'NVMeoF'.
-	nvmeoFSecrets []string
+	nvmeoFSecrets string
 	// Oem shall contain the OEM extensions. All values for properties that this object contains shall conform to the
 	// Redfish Specification-described requirements.
 	OEM json.RawMessage `json:"Oem"`
@@ -39,8 +39,8 @@ func (keyservice *KeyService) UnmarshalJSON(b []byte) error {
 	type temp KeyService
 	var t struct {
 		temp
-		NVMeoFKeyPolicies common.LinksCollection
-		NVMeoFSecrets     common.LinksCollection
+		NVMeoFKeyPolicies common.Link
+		NVMeoFSecrets     common.Link
 	}
 
 	err := json.Unmarshal(b, &t)
@@ -51,52 +51,20 @@ func (keyservice *KeyService) UnmarshalJSON(b []byte) error {
 	*keyservice = KeyService(t.temp)
 
 	// Extract the links to other entities for later
-	keyservice.nvmeoFKeyPolicies = t.NVMeoFKeyPolicies.Members.ToStrings()
-	keyservice.nvmeoFSecrets = t.NVMeoFSecrets.Members.ToStrings()
+	keyservice.nvmeoFKeyPolicies = t.NVMeoFKeyPolicies.String()
+	keyservice.nvmeoFSecrets = t.NVMeoFSecrets.String()
 
 	return nil
 }
 
 // NVMeoFKeyPolicies gets the NVMe-oF key policies maintained by this service.
 func (keyservice *KeyService) NVMeoFKeyPolicies() ([]*KeyPolicy, error) {
-	var result []*KeyPolicy
-
-	collectionError := common.NewCollectionError()
-	for _, uri := range keyservice.nvmeoFKeyPolicies {
-		unit, err := GetKeyPolicy(keyservice.GetClient(), uri)
-		if err != nil {
-			collectionError.Failures[uri] = err
-		} else {
-			result = append(result, unit)
-		}
-	}
-
-	if collectionError.Empty() {
-		return result, nil
-	}
-
-	return result, collectionError
+	return ListReferencedKeyPolicys(keyservice.GetClient(), keyservice.nvmeoFKeyPolicies)
 }
 
 // NVMeofSecrets gets the NVMe-oF keys maintained by this service.
 func (keyservice *KeyService) NVMeoFSecrets() ([]*Key, error) {
-	var result []*Key
-
-	collectionError := common.NewCollectionError()
-	for _, uri := range keyservice.nvmeoFSecrets {
-		unit, err := GetKey(keyservice.GetClient(), uri)
-		if err != nil {
-			collectionError.Failures[uri] = err
-		} else {
-			result = append(result, unit)
-		}
-	}
-
-	if collectionError.Empty() {
-		return result, nil
-	}
-
-	return result, collectionError
+	return ListReferencedKeys(keyservice.GetClient(), keyservice.nvmeoFSecrets)
 }
 
 // GetKeyService will get a KeyService instance from the service.
