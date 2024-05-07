94edfe616af139bd0cb36289f2b3f15481fc354f
Sean McGinnis <sean.mcginnis@gmail.com>
2024-05-07 13:58:05 -0500

    Workaround for NVIDIA PowerSubsystem issue
    Their implmentation incorrectly returns PowerSubsystem.Attributes as an
    array, causing unmarshal errors. This works around that by skipping it.
    
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>

--- redfish/powersubsystem_test.go
+++ redfish/powersubsystem_test.go
@@ -49,6 +49,56 @@ var powerSubsystemBody = `{
 	"@odata.id": "/redfish/v1/Chassis/1U/PowerSubsystem"
   }`
 
+var powerSubsystemNVBody = `{
+	"@odata.context": "/redfish/v1/$metadata#PowerSubsystem.PowerSubsystem",
+	"@odata.etag": "\"1715094363\"",
+	"@odata.id": "/redfish/v1/Chassis/DGX/PowerSubsystem",
+	"@odata.type": "#PowerSubsystem.v1_1_0.PowerSubsystem",
+	"Allocation": [],
+	"Description": "PowerSubsytem for this Chassis",
+	"Id": "PowerSubsytem",
+	"Name": "PowerSubsytem",
+	"PowerSupplies": {
+	  "@odata.id": "/redfish/v1/Chassis/DGX/PowerSubsystem/PowerSupplies"
+	},
+	"PowerSupplyRedundancy": [
+	  {
+		"MaxSupportedInGroup": 6,
+		"MinNeededInGroup": 1,
+		"RedundancyGroup": [
+		  {
+			"@odata.id": "/redfish/v1/Chassis/DGX/PowerSubsystem/PowerSupplies/PSU0"
+		  },
+		  {
+			"@odata.id": "/redfish/v1/Chassis/DGX/PowerSubsystem/PowerSupplies/PSU1"
+		  },
+		  {
+			"@odata.id": "/redfish/v1/Chassis/DGX/PowerSubsystem/PowerSupplies/PSU2"
+		  },
+		  {
+			"@odata.id": "/redfish/v1/Chassis/DGX/PowerSubsystem/PowerSupplies/PSU3"
+		  },
+		  {
+			"@odata.id": "/redfish/v1/Chassis/DGX/PowerSubsystem/PowerSupplies/PSU4"
+		  },
+		  {
+			"@odata.id": "/redfish/v1/Chassis/DGX/PowerSubsystem/PowerSupplies/PSU5"
+		  }
+		],
+		"RedundancyGroup@odata.count": 6,
+		"RedundancyType": "NPlusM",
+		"Status": {
+		  "Health": "OK",
+		  "State": "Enabled"
+		}
+	  }
+	],
+	"Status": {
+	  "Health": "OK",
+	  "State": "Enabled"
+	}
+  }`
+
 // TestPowerSubsystem tests the parsing of PowerSubsystem objects.
 func TestPowerSubsystem(t *testing.T) {
 	var result PowerSubsystem
@@ -66,4 +116,14 @@ func TestPowerSubsystem(t *testing.T) {
 	assertEquals(t, "Failover", string(result.PowerSupplyRedundancy[0].RedundancyType))
 	assertEquals(t, "/redfish/v1/Chassis/1U/PowerSubsystem/PowerSupplies/Bay2", result.PowerSupplyRedundancy[0].redundancyGroup[1])
 	assertEquals(t, "/redfish/v1/Chassis/1U/PowerSubsystem/PowerSupplies", result.powerSupplies)
+}
+
+// TestPowerSubsystemNVWorkaround tests the workaround for a non-spec implementation of PowerSubsystem objects.
+func TestPowerSubsystemNVWorkaround(t *testing.T) {
+	var result PowerSubsystem
+	err := json.NewDecoder(strings.NewReader(powerSubsystemNVBody)).Decode(&result)
+
+	if err != nil {
+		t.Errorf("Error decoding JSON: %s", err)
+	}
 }
