05fa43a5dc9ebbf1f5e042bcb1ddeba416474627
Sean McGinnis <sean.mcginnis@gmail.com>
2024-10-15 10:28:58 -0500

    Add Supermicro OEM Chassis object
    This adds an OEM-specific Chassis that enables accessing the OEM
    properties of a chassis.
    
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>

---
+++ oem/smc/chassis.go
@@ -0,0 +1,48 @@
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//
+
+package smc
+
+import (
+	"encoding/json"
+
+	"github.com/stmcginnis/gofish/redfish"
+)
+
+// Chassis is a Supermicro OEM instance of a Chassis.
+type Chassis struct {
+	redfish.Chassis
+	BoardSerialNumber string
+	GUID              string
+	BoardID           string
+}
+
+// FromChassis converts a standard Chassis object to the OEM implementation.
+func FromChassis(chassis *redfish.Chassis) (*Chassis, error) {
+	cs := Chassis{
+		Chassis: *chassis,
+	}
+
+	var t struct {
+		Oem struct {
+			Supermicro struct {
+				BoardSerialNumber string `json:"BoardSerialNumber"`
+				GUID              string `json:"GUID"`
+				BoardID           string `json:"BoardID"`
+			} `json:"Supermicro"`
+		} `json:"Oem"`
+	}
+
+	err := json.Unmarshal(chassis.RawData, &t)
+	if err != nil {
+		return nil, err
+	}
+
+	cs.BoardSerialNumber = t.Oem.Supermicro.BoardSerialNumber
+	cs.GUID = t.Oem.Supermicro.GUID
+	cs.BoardID = t.Oem.Supermicro.BoardID
+
+	cs.SetClient(chassis.GetClient())
+	return &cs, nil
+}
