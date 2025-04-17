3e55df832d243f8d30fdfed4db8fc57442ef7894
Manuel Mendez <git@i.m.mmlb.dev>
2025-04-17 13:49:26 -0400

    virtualmedia: Return Tasks if present in InsertMedia http response

--- redfish/virtualmedia_test.go
+++ redfish/virtualmedia_test.go
@@ -166,7 +166,7 @@ func TestVirtualMediaInsert(t *testing.T) {
 	testClient := &common.TestClient{}
 	result.SetClient(testClient)
 
-	err = result.InsertMedia("https://example.com/image", false, true)
+	_, err = result.InsertMedia("https://example.com/image", false, true)
 
 	if err != nil {
 		t.Errorf("Error making InsertMedia call: %s", err)
@@ -207,7 +207,7 @@ func TestVirtualMediaInsertConfig(t *testing.T) {
 		WriteProtected: true,
 	}
 
-	err = result.InsertMediaConfig(virtualMediaConfig)
+	_, err = result.InsertMediaConfig(virtualMediaConfig)
 	if err != nil {
 		t.Errorf("Error making InsertMediaConfig call: %s", err)
 	}
