10c58848e5211fa407afe22a951009092d1efaa4
Manuel Mendez <git@i.m.mmlb.dev>
2025-05-13 11:12:42 -0400

    Add common/ActionInfo and co types

--- common/action.go
+++ common/action.go
@@ -1,5 +1,10 @@
 package common
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 // ActionTarget is contains the target endpoint for object Actions.
 //
 // Deprecated: Action should be used instead.
@@ -11,4 +16,98 @@ type ActionTarget struct {
 type Action struct {
 	Target string
 	Info   string `json:"@Redfish.ActionInfo"`
+}
+
+type ParameterDataType string
+
+const (
+	ParameterDataTypeBoolean     ParameterDataType = "Boolean"
+	ParameterDataTypeNumber      ParameterDataType = "Number"
+	ParameterDataTypeNumberArray ParameterDataType = "NumberArray"
+	ParameterDataTypeString      ParameterDataType = "String"
+	ParameterDataTypeStringArray ParameterDataType = "StringArray"
+	ParameterDataTypeObject      ParameterDataType = "Object"
+	ParameterDataTypeObjectArray ParameterDataType = "ObjectArray"
+)
+
+// UnmarshalJSON unmarshals a DataType
+func (pdt *ParameterDataType) UnmarshalJSON(b []byte) error {
+	var s string
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
+	}
+
+	t := ParameterDataType(s)
+
+	switch t {
+	case ParameterDataTypeBoolean,
+		ParameterDataTypeNumber,
+		ParameterDataTypeNumberArray,
+		ParameterDataTypeString,
+		ParameterDataTypeStringArray,
+		ParameterDataTypeObject,
+		ParameterDataTypeObjectArray:
+		*pdt = t
+		return nil
+	default:
+		return fmt.Errorf("Unknown DataType specified: %s", s)
+	}
+}
+
+type Parameter struct {
+	Name            string // required
+	Required        bool
+	DataType        ParameterDataType
+	ObjectDataType  string
+	AllowableValues []string
+}
+
+// UnmarshalJSON unmarshals an ActionInfo
+func (p *Parameter) UnmarshalJSON(b []byte) error {
+	var t struct {
+		Name            string // required
+		Required        bool
+		DataType        ParameterDataType
+		ObjectDataType  string
+		AllowableValues []string
+	}
+	err := json.Unmarshal(b, &t)
+	if err != nil {
+		return err
+	}
+
+	if t.Name == "" {
+		return fmt.Errorf("Parameter required field Name is missing")
+	}
+
+	*p = t
+	return nil
+}
+
+type ActionInfo struct {
+	ResourceCollection // Name and Id fields are required
+	Parameters         []Parameter
+}
+
+// UnmarshalJSON unmarshals an ActionInfo
+func (ai *ActionInfo) UnmarshalJSON(b []byte) error {
+	var t struct {
+		ResourceCollection
+		Parameters []Parameter
+	}
+
+	err := json.Unmarshal(b, &t)
+	if err != nil {
+		return err
+	}
+
+	if t.Name == "" {
+		return fmt.Errorf("ActionInfo required field Name is missing")
+	}
+	if t.ID == "" {
+		return fmt.Errorf("ActionInfo required field ID is missing")
+	}
+
+	*ai = t
+	return nil
 }
