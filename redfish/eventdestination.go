ccfac8742da2ceb3a49e491ad36bdf8f816b5509
Sean McGinnis <sean.mcginnis@gmail.com>
2025-11-26 17:25:06 -0500

    Add support for idref EventDestination.OriginResources (#480)
    Handles idrefs rather than an array of strings.
    
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>

--- redfish/eventdestination.go
+++ redfish/eventdestination.go
@@ -352,7 +352,24 @@ func (eventdestination *EventDestination) UnmarshalJSON(b []byte) error {
 
 	err := json.Unmarshal(b, &t)
 	if err != nil {
-		return err
+		// Workaround for the OriginResources actually being an idref
+		var x struct {
+			temp
+			OriginResources    common.Links
+			Certificates       common.Link
+			ClientCertificates common.Link
+		}
+
+		err2 := json.Unmarshal(b, &x)
+		if err2 != nil {
+			// That didn't work either, just return the original error
+			return err
+		}
+
+		t.temp = x.temp
+		t.Certificates = x.Certificates
+		t.ClientCertificates = x.ClientCertificates
+		t.OriginResources = x.OriginResources.ToStrings()
 	}
 
 	// Extract the links to other entities for later
